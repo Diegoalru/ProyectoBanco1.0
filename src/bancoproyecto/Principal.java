b9cfe41b712d5318d8291ff324a4762c3b3c923e
DuchetCR <56007114+DuchetCR@users.noreply.github.com>
2023-07-07 16:06:23 -0600

    Update3, censura contraseña

--- src/bancoproyecto/Principal.java
+++ src/bancoproyecto/Principal.java
@@ -6,40 +6,40 @@ package bancoproyecto;
 
 
 import javax.swing.JOptionPane;
-
-
+import javax.swing.JPasswordField;
 /**
  *
  * @author DuchetCR
  */
 public class Principal {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         Usuario u1 = new Usuario();
         u1.setUsuario(JOptionPane.showInputDialog("Cree su Usuario: "));
         u1.setContraseña();
         System.out.println(u1.getUsuario());
         System.out.println(u1.getContraseña());
+        
         for (int i = 0; i < 3; i++) {
             String usuario = JOptionPane.showInputDialog("Usuario: ");
-            String password = JOptionPane.showInputDialog("Password: ");
-        if (usuario.equals(u1.getUsuario()) && password.equals(u1.getContraseña())) {
-            JOptionPane.showMessageDialog(null,"Entro al sistema");
-            // Resto del código aquí
-   
-            
-            
-            
-            
-            break;
-        } else if (i < 2) {
-            JOptionPane.showMessageDialog(null,"Usuario rechazado, intenta nuevamente");
-        } else if (i == 2) {
-            JOptionPane.showMessageDialog(null,"Usuario bloqueado, vuelve a intentar más tarde");
-}
+            JPasswordField passwordField = new JPasswordField();
+            passwordField.setEchoChar('*');
+            int option = JOptionPane.showConfirmDialog(null, passwordField, "Password:", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
+            if (option == JOptionPane.OK_OPTION && usuario.equals(u1.getUsuario()) && passwordField.getPassword().length > 0) {
+                JOptionPane.showMessageDialog(null, "Entro al sistema");
+                // Resto del código aquí
+                
+                
+                
+                
+                
+                
+                break;
+            } else if (i < 2) {
+                JOptionPane.showMessageDialog(null, "Usuario rechazado, intenta nuevamente");
+            } else if (i == 2) {
+                JOptionPane.showMessageDialog(null, "Usuario bloqueado, vuelve a intentar más tarde");
+            }
         }
     }
-}  
+}
