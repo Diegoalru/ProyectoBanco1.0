e90eedb5eb3e6320fa1725f6ed9875ff3cfaa206
Diego Rubí Salas <diegoalru@gmail.com>
2023-07-11 11:59:25 -0600

    Inicio de app mediante el controlador principal.

--- src/bancoproyecto/Principal.java
+++ src/bancoproyecto/Principal.java
@@ -1,8 +1,6 @@
 package bancoproyecto;
 
-import bancoproyecto.data.UsuarioData;
-import bancoproyecto.models.Usuario;
-import bancoproyecto.view.InicioView;
+import bancoproyecto.controller.MainController;
 
 import javax.swing.*;
 import java.util.logging.Level;
@@ -16,31 +14,16 @@ public class Principal {
     private static final Logger logger = Logger.getLogger(Principal.class.getName());
 
     public static void main(String[] args) {
-        Start();
-    }
-
-    /**
-     * Método que inicia la interfaz GUI.
-     */
-    private static void Start() {
-        logger.log(Level.INFO, "Iniciando programa");
-
-        InsertaUsuariosDePrueba();
-
         SwingUtilities.invokeLater(() -> {
-            InicioView inicioView = new InicioView();
-            inicioView.setVisible(true);
+            try {
+                // Establece el look and feel de la aplicación.
+                UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+            } catch (Exception e) {
+                logger.severe("No se pudo establecer el look and feel");
+            }
+
+            logger.log(Level.INFO, "Iniciando programa...");
+            MainController.OpenGUI();
         });
-    }
-
-    private static void InsertaUsuariosDePrueba() {
-        // Crea un usuario automáticamente.
-        Usuario nuevoUsuario1 = new Usuario("user1", "123456789");
-        Usuario nuevoUsuario2 = new Usuario("user2", "123456789");
-
-        // Guarda los usuarios en UsuarioData.
-        UsuarioData.NuevoUsuario(nuevoUsuario1, nuevoUsuario2);
-
-        logger.log(Level.INFO, "Usuarios registrados en el sistema: {0}", UsuarioData.ObtieneListaUsuarios());
     }
 }
