26458889c72b5a8b2ab8911a11bf9c8beb61d945
Diego Rubí Salas <diegoalru@gmail.com>
2023-07-11 19:52:23 -0600

    Cambio de Class a Record.

--- src/bancoproyecto/models/Usuario.java
+++ src/bancoproyecto/models/Usuario.java
@@ -1,33 +1,8 @@
 package bancoproyecto.models;
 
-public class Usuario {
-
-    private String usuario;
-    private String contrasena;
-
-    public Usuario(String usuario, String contrasena) {
-        this.usuario = usuario;
-        this.contrasena = contrasena;
-    }
-
-    public String getUsuario() {
-        return this.usuario;
-    }
-
-    public void setUsuario(String usuario) {
-        this.usuario = usuario;
-    }
-
-    public String getContrasena() {
-        return this.contrasena;
-    }
-
-    public void setContrasena(String nuevaContrasena) {
-        this.contrasena = nuevaContrasena;
-    }
-    
+public record Usuario(String name, String username, String password) {
     @Override
-    public String toString(){
-        return String.format("Usuario: %s, y su contraseña: %s", usuario, contrasena); // %s es para string.
+    public String toString() {
+        return String.format("Name: %s, Username: %s, Password: %s", name, username, password);
     }
 }
