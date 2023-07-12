7e436ba689e18dca594a319577800a6e0777a872
Diego Rubí Salas <diegoalru@gmail.com>
2023-07-11 22:44:51 -0600

    Uso de expresiones lambda y funciones de validación.

--- src/bancoproyecto/controller/UserController.java
+++ src/bancoproyecto/controller/UserController.java
@@ -9,6 +9,7 @@ import bancoproyecto.models.UserRegister;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class UserController {
     private final int STR_MIN_LENGTH = 5;
@@ -21,30 +22,18 @@ public class UserController {
      */
     public void RegistraUsuario(UserRegister usuario) throws Exception {
         //#region Validaciones
-        if (usuario.username().length() < STR_MIN_LENGTH) {
-            throw new Exception("El usuario debe tener al menos %d caracteres".formatted(STR_MIN_LENGTH));
-        }
-
-        if (!usuario.username().matches("[a-zA-Z0-9]+")) {
-            throw new Exception("El usuario no puede contener caracteres especiales");
-        }
-
-        if (usuario.password().length() < STR_MIN_LENGTH) {
-            throw new Exception("La contraseña debe tener al menos %d caracteres".formatted(STR_MIN_LENGTH));
-        }
-
-        if (usuario.password().equals(usuario.username())) {
-            throw new Exception("La contraseña no puede ser igual al usuario");
-        }
-
-        if (usuario.password().matches(".*([a-zA-Z])\\1+.*")) {
-            throw new Exception("La contrasena no puede tener caracteres iguales consecutivos");
-        }
-
-        // La contraseña puede tener caracteres especiales pero no espacios y solo $, #, %, &, *.
-        if (!usuario.password().matches("^[a-zA-Z0-9$#%&*]+$")) {
-            throw new Exception("La contraseña solo puede tener los siguientes caracteres especiales: $, #, %, &, *");
-        }
+        validar(usuario.username(), "El usuario debe tener al menos %d caracteres".formatted(STR_MIN_LENGTH),
+                s -> s.length() >= STR_MIN_LENGTH);
+        validar(usuario.username(), "El usuario no puede contener caracteres especiales",
+                s -> s.matches("[a-zA-Z0-9]+"));
+        validar(usuario.password(), "La contraseña debe tener al menos %d caracteres".formatted(STR_MIN_LENGTH),
+                s -> s.length() >= STR_MIN_LENGTH);
+        validar(usuario.password(), "La contraseña no puede ser igual al usuario",
+                s -> !s.equals(usuario.username()));
+        validar(usuario.password(), "La contrasena no puede tener caracteres iguales consecutivos",
+                s -> !s.matches(".*([a-zA-Z])\\1+.*"));
+        validar(usuario.password(), "La contraseña solo puede tener los siguientes caracteres especiales: $, #, %, &, *",
+                s -> s.matches("^[a-zA-Z0-9$#%&*]+$"));
         //#endregion
 
         UserModel usuarioModel = new UserModel(usuario.name(), usuario.username(), usuario.password());
@@ -64,37 +53,25 @@ public class UserController {
      */
     public void InicioSesion(UserLogin usuario) throws Exception {
         //#region Validaciones
-        if (usuario.username().length() < STR_MIN_LENGTH) {
-            throw new Exception("El usuario debe tener al menos %d caracteres".formatted(STR_MIN_LENGTH));
-        }
-
-        if (!usuario.username().matches("[a-zA-Z0-9]+")) {
-            throw new Exception("El usuario no puede contener caracteres especiales");
-        }
-
-        if (usuario.password().length() < STR_MIN_LENGTH) {
-            throw new Exception("La contraseña debe tener al menos %d caracteres".formatted(STR_MIN_LENGTH));
-        }
-
-        if (usuario.password().equals(usuario.username())) {
-            throw new Exception("La contraseña no puede ser igual al usuario");
-        }
-
-        if (usuario.password().matches(".*([a-zA-Z])\\1+.*")) {
-            throw new Exception("La contrasena no puede tener caracteres iguales consecutivos");
-        }
-
-        // La contraseña puede tener caracteres especiales pero no espacios y solo $, #, %, &, *.
-        if (!usuario.password().matches("^[a-zA-Z0-9$#%&*]+$")) {
-            throw new Exception("La contraseña solo puede tener los siguientes caracteres especiales: $, #, %, &, *");
-        }
+        validar(usuario.username(), "El usuario debe tener al menos %d caracteres".formatted(STR_MIN_LENGTH),
+                s -> s.length() >= STR_MIN_LENGTH);
+        validar(usuario.username(), "El usuario no puede contener caracteres especiales",
+                s -> s.matches("[a-zA-Z0-9]+"));
+        validar(usuario.password(), "La contraseña debe tener al menos %d caracteres".formatted(STR_MIN_LENGTH),
+                s -> s.length() >= STR_MIN_LENGTH);
+        validar(usuario.password(), "La contraseña no puede ser igual al usuario",
+                s -> !s.equals(usuario.username()));
+        validar(usuario.password(), "La contrasena no puede tener caracteres iguales consecutivos",
+                s -> !s.matches(".*([a-zA-Z])\\1+.*"));
+        validar(usuario.password(), "La contraseña solo puede tener los siguientes caracteres especiales: $, #, %, &, *",
+                s -> s.matches("^[a-zA-Z0-9$#%&*]+$"));
         //#endregion
 
         UserModel usuarioModel = new UserModel(usuario.username(), usuario.password());
         UserModel resultLogin = UserRepository.Login(usuarioModel);
 
         if (resultLogin == null) {
-            throw new Exception("El usuario o la contraseña son incorrectos");
+            throw new Exception("Username or password incorrect!");
         }
 
         List<Account> accounts = new ArrayList<>();
@@ -108,6 +85,7 @@ public class UserController {
 
     /**
      * Lista de usuarios en UsuarioData
+     *
      * @return Usuario que se encuentra en UsuarioData
      * @deprecated Solo para pruebas
      */
@@ -115,5 +93,11 @@ public class UserController {
         var users = UserRepository.GetUsers().toString();
         System.out.println(users);
         return users;
+    }
+
+    private void validar(String value, String errorMessage, Predicate<String> condition) throws Exception {
+        if (!condition.test(value)) {
+            throw new Exception(errorMessage);
+        }
     }
 }
